c9ae5704f0895440ef0f22767ee7880136a3dff5
nftdejita <108794868+nftdejita@users.noreply.github.com>
2023-05-24 08:29:17 +0900

    Update OnePonziMain.jsx

--- src/components/OnePonziMain.jsx
+++ src/components/OnePonziMain.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import {
   Button,
   Input,
@@ -9,18 +8,6 @@ import {
 } from "@mui/material";
 
 const OnePonziMain = ({ contract, account, updateLog, web3 }) => {
-  const [investAmount, setInvestAmount] = useState("0.1");
-
-  async function invest() {
-    try {
-      const amount = web3.utils.toWei(investAmount, "ether");
-      await contract.methods.invest().send({ from: account, value: amount });
-      updateLog(`Invest ${web3.utils.fromWei(amount, "ether")} ether.`);
-    } catch (error) {
-      updateLog(`Error: ${error.message}`);
-    }
-  }
-
   return (
     <Card sx={{ margin: "6px" }}>
       <CardContent
@@ -34,18 +21,6 @@ const OnePonziMain = ({ contract, account, updateLog, web3 }) => {
         <Typography variant="h4" color="inherit" noWrap>
           OnePonzi
         </Typography>
-        <Box sx={{ display: "flex", gap: 1 }}>
-          <Input
-            type="number"
-            value={investAmount}
-            onChange={(e) => setInvestAmount(e.target.value)}
-            min="0.1"
-            step="0.1"
-          />
-          <Button variant="contained" onClick={invest}>
-            投資
-          </Button>
-        </Box>
       </CardContent>
     </Card>
   );
