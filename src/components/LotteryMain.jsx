704b8851568d053d72fa5548b1d2575dda012a84
nftdejita <108794868+nftdejita@users.noreply.github.com>
2023-05-24 08:28:08 +0900

    Update LotteryMain.jsx

--- src/components/LotteryMain.jsx
+++ src/components/LotteryMain.jsx
@@ -8,27 +8,7 @@ import {
   Typography,
 } from "@mui/material";
 
-const LotteryMain = ({ contract, account, updateLog, web3 }) => {
-  async function ticketBuy() {
-    try {
-      const amount = web3.utils.toWei("0.01", "ether");
-      await contract.methods.ticketbuy().send({ from: account, value: amount });
-      updateLog(`Buy Ticket ${web3.utils.fromWei(amount, "ether")} ether.`);
-    } catch (error) {
-      updateLog(`Error: ${error.message}`);
-    }
-  }
-
-  async function drawWinner() {
-    try {
-      const result = await contract.methods
-        .drawWinner()
-        .send({ from: account });
-      updateLog(`drawWinner `);
-    } catch (error) {
-      updateLog(`Error: ${error.message}`);
-    }
-  }
+const LotteryMain = () => {
 
   return (
     <Card sx={{ margin: "6px" }}>
@@ -43,15 +23,6 @@ const LotteryMain = ({ contract, account, updateLog, web3 }) => {
         <Typography variant="h4" color="inherit" noWrap>
           Lottery
         </Typography>
-        <contents>
-          <Button variant="contained" onClick={ticketBuy}>
-            宝くじ購入
-          </Button>
-          　
-          <Button variant="contained" onClick={drawWinner}>
-            抽選
-          </Button>
-        </contents>
       </CardContent>
     </Card>
   );
